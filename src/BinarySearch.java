8585ec13f887dc4dfa7acc93a97abde8df6a27e5
tvivlad2 <tvi-vlad88@yandex.ru>
2025-10-14 20:53:32 +0500

    BinarySearch class created

---
+++ src/BinarySearch.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class BinarySearch<T>{
+
+    public T search(ArrayList <? extends T> list, T obj, Comparator<? super T> comparator){
+
+        int firstIndex = 0;
+        int lastIndex = list.size()-1;
+
+        int middle;
+        while (firstIndex<=lastIndex){
+            middle = (firstIndex+lastIndex)/2;
+
+            int compareResult = comparator.compare(obj,list.get(middle));
+            if (compareResult==0){
+                return list.get(middle);
+            }
+            if (compareResult>0){
+                firstIndex = middle+1;
+            }
+            if (compareResult<0){
+                lastIndex = middle-1;
+            }
+
+        }
+        return null;
+    }
+}
